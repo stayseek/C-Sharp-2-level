e5016f604b345f7c6415d7d0b5678c68c7b3a9ef
stayseek <stayseek0@gmail.com>
2018-11-29 11:46:25 +0300

    Добавлен свой класс Planet, графически представленный изображением.

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Продвинутый курс C#
+//Урок 1. Объектно-ориентированное программирование. Часть 1
+//Домашнее задание.
+//Автор: Станислав Митрофанов
+
+using System;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+        }
+    }
+}
